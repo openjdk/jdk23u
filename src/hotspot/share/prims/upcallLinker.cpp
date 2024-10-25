121d764b5092b7e92d6b394a75da8e3dc621aeec
Jorn Vernee <jorn.vernee@oracle.com>
2024-10-25 21:00:03 +0000

    Remove code unrelated to fix

--- src/hotspot/share/prims/upcallLinker.cpp
+++ src/hotspot/share/prims/upcallLinker.cpp
@@ -137,10 +137,11 @@ void UpcallLinker::on_exit(UpcallStub::FrameData* context) {
 }
 
 void UpcallLinker::handle_uncaught_exception(oop exception) {
+  ResourceMark rm;
+  // Based on CATCH macro
   tty->print_cr("Uncaught exception:");
-  Handle exception_h(Thread::current(), exception);
-  java_lang_Throwable::print_stack_trace(exception_h, tty);
-  fatal("Unrecoverable uncaught exception encountered");
+  exception->print();
+  ShouldNotReachHere();
 }
 
 JVM_ENTRY(jlong, UL_MakeUpcallStub(JNIEnv *env, jclass unused, jobject mh, jobject abi, jobject conv,
